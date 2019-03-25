55dff76c5ae81bd436360e6cbf5adeaff56440f0
Ben Johnson <ben@indiehd.com>
2019-03-25 12:30:22 -0400

    refact: Type-hint the interface instead of the implementation

--- src/ProcessFailedException.php
+++ src/ProcessFailedException.php
@@ -4,9 +4,11 @@ namespace IndieHD\AudioManipulator;
 
 use Symfony\Component\Process\Exception\ProcessFailedException as SymfonyProcessFailedException;
 
+use IndieHD\AudioManipulator\ProcessInterface;
+
 class ProcessFailedException extends SymfonyProcessFailedException
 {
-    public function __construct(Process $process)
+    public function __construct(ProcessInterface $process)
     {
         throw new SymfonyProcessFailedException($process->getProcess());
     }
