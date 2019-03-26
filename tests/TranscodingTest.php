24cc64affbd027b0eb0ea874517c88d0b1d510a2
Ben Johnson <ben@indiehd.com>
2019-03-26 14:38:41 -0400

    tests: Add tests for two exception scenarios
    Also, change requirement for return types in a couple of instances.

--- tests/TranscodingTest.php
+++ tests/TranscodingTest.php
@@ -4,6 +4,10 @@ namespace IndieHD\AudioManipulator;
 
 use PHPUnit\Framework\TestCase;
 
+use Symfony\Component\Filesystem\Exception\FileNotFoundException;
+
+use IndieHD\AudioManipulator\Validation\InvalidAudioFileException;
+
 class TranscodingTest extends TestCase
 {
     /**
@@ -12,7 +16,7 @@ class TranscodingTest extends TestCase
     public function setUp(): void
     {
         $this->transcoder = app()->builder->get('transcoder');
-        
+
         $this->testDir = __DIR__ . DIRECTORY_SEPARATOR . 'samples' . DIRECTORY_SEPARATOR;
     }
 
@@ -20,12 +24,12 @@ class TranscodingTest extends TestCase
     {
         $this->testDir =
 
-        $this->assertTrue($this->transcoder->transcode(
+        $this->assertIsArray($this->transcoder->transcode(
             $this->testDir . 'foo.flac',
             $this->testDir . 'foo.mp3'
-        )['result']);
+        ));
     }
-    
+
     public function testConvertingWavToFlacSucceeds()
     {
         $this->assertIsArray($this->transcoder->convertWavToFlac(
@@ -33,14 +37,14 @@ class TranscodingTest extends TestCase
             $this->testDir . 'foo.flac'
         )['result']);
     }
-    
+
     public function testTranscodingWavToMp3Succeeds()
     {
-        $this->assertTrue($this->transcoder->transcode(
+        $this->assertIsArray($this->transcoder->transcode(
             $this->testDir . 'foo.flac',
             $this->testDir . 'foo.wav'
-        )['result']);
-        
+        ));
+
         $this->assertTrue($this->transcoder->wavToMp3(
             $this->testDir . 'foo.wav',
             $this->testDir . 'foo.mp3',
@@ -48,11 +52,31 @@ class TranscodingTest extends TestCase
             128
         )['result']);
     }
-    
+
     public function testConvertingFlacToAlacSucceeds()
     {
         $this->assertTrue($this->transcoder->transcodeFlacToAlac(
             $this->testDir . 'foo.flac'
         )['result']);
+    }
+
+    public function testExceptionIsThrownWhenInputFileDoesNotExist()
+    {
+        $this->expectException(FileNotFoundException::class);
+
+        $this->transcoder->transcode(
+            $this->testDir . 'bar.flac',
+            $this->testDir . 'bar.wav'
+        );
+    }
+
+    public function testExceptionIsThrownWhenInputFileTypeIsInvalid()
+    {
+        $this->expectException(InvalidAudioFileException::class);
+
+        $this->transcoder->transcode(
+            $this->testDir . 'foo.txt',
+            $this->testDir . 'foo.wav'
+        );
     }
 }
