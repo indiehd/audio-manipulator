e5b5a74175852bcf760a9fdbe7618b84ff8b0027
Ben Johnson <ben@indiehd.com>
2019-04-09 14:02:44 -0400

    tests: Further-refine SoX command requirements

--- tests/CliCommand/SoxCommandTest.php
+++ tests/CliCommand/SoxCommandTest.php
@@ -8,12 +8,21 @@ use IndieHD\AudioManipulator\CliCommand\SoxCommand;
 
 class SoxCommandTest extends TestCase
 {
+    private $soxCommand;
+
     /**
      * @inheritdoc
      */
     public function setUp(): void
     {
         $this->soxCommand = new SoxCommand();
+    }
+
+    public function testWhenInstantiatedRequiredPropertiesAreSet()
+    {
+        $this->assertIsString($this->soxCommand->getName());
+        $this->assertIsString($this->soxCommand->getBinary());
+        $this->assertIsArray($this->soxCommand->getCommandParts());
     }
 
     public function testCommandIsComposedCorrectly()
@@ -26,7 +35,9 @@ class SoxCommandTest extends TestCase
 
         $this->soxCommand->addArgument('infile', 'test.flac');
 
-        $this->soxCommand->addArgument('fopts-out', 'test.wav');
+        $this->soxCommand->addArgument('outfile', 'test.wav');
+
+        $this->soxCommand->addArgument('fopts-out', '--no-clobber');
 
         $this->soxCommand->addArgument('fopts-in', '--channels 2');
 
@@ -35,7 +46,21 @@ class SoxCommandTest extends TestCase
         $this->soxCommand->addArgument('gopts', '--single-threaded');
 
         $this->assertEquals(
-            'sox --single-threaded --channels 2 test.flac test.wav fade q 0.5 1 0.5',
+            'sox --single-threaded --channels 2 test.flac --no-clobber test.wav fade q 0.5 1 0.5',
+            implode(' ', $this->soxCommand->compose())
+        );
+    }
+
+    public function testFadeCommandIsComposedCorrectly()
+    {
+        $this->soxCommand->addArgument('infile', 'test.flac');
+
+        $this->soxCommand->addArgument('outfile', 'faded.flac');
+
+        $this->soxCommand->fade('q', 0.5, 1, 0.5);
+
+        $this->assertEquals(
+            'sox test.flac faded.flac fade q 0.5 1 0.5',
             implode(' ', $this->soxCommand->compose())
         );
     }
