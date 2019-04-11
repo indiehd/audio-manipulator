1712ebb86207c2b50c460bb9e3a2f7d44b0d440d
Ben Johnson <ben@indiehd.com>
2019-04-11 10:27:51 -0400

    fix: Add missing method signatures

--- src/CliCommand/FfmpegCommandInterface.php
+++ src/CliCommand/FfmpegCommandInterface.php
@@ -6,5 +6,11 @@ use IndieHD\AudioManipulator\CliCommand\CliCommandInterface;
 
 interface FfmpegCommandInterface extends CliCommandInterface
 {
+    public function input(string $inputFile): void;
 
+    public function output(string $outputFile): void;
+
+    public function overwriteOutput(): void;
+
+    public function forceAudioCodec(string $codec): void;
 }
