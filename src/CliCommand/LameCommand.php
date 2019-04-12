82c51b018068687bf003e065e5d5cea5ff6775a6
Ben Johnson <ben@indiehd.com>
2019-04-12 10:30:10 -0400

    feat: Add support for most common and useful lame commands

--- src/CliCommand/LameCommand.php
+++ src/CliCommand/LameCommand.php
@@ -32,4 +32,54 @@ class LameCommand extends CliCommand implements LameCommandInterface
     {
         $this->addArgument('outfile', escapeshellarg($outputFile));
     }
+
+    public function quiet(): void
+    {
+        $this->addArgument('options', '--quiet');
+    }
+
+    public function enableAndForceLameTag(): void
+    {
+        $this->addArgument('options', '-T');
+    }
+
+    public function noReplayGain(): void
+    {
+        $this->addArgument('options', '--noreplaygain');
+    }
+
+    public function quality(int $quality): void
+    {
+        $this->addArgument('options', '--q ' . $quality);
+    }
+
+    public function resample(float $frequency): void
+    {
+        $this->addArgument('options', '--resample ' . $frequency);
+    }
+
+    public function bitwidth(int $width): void
+    {
+        $this->addArgument('options', '--bitwidth ' . $width);
+    }
+
+    public function cbr(): void
+    {
+        $this->addArgument('options', '--cbr');
+    }
+
+    public function bitrate(int $rate): void
+    {
+        $this->addArgument('options', '-b ' . $rate);
+    }
+
+    public function abr(): void
+    {
+        $this->addArgument('options', '--abr');
+    }
+
+    public function vbr(int $quality): void
+    {
+        $this->addArgument('options', '--vbr-new -V ' . $quality);
+    }
 }
--- src/CliCommand/LameCommandInterface.php
+++ src/CliCommand/LameCommandInterface.php
@@ -9,4 +9,24 @@ interface LameCommandInterface extends CliCommandInterface
     public function input(string $inputFile): void;
 
     public function output(string $outputFile): void;
+
+    public function quiet(): void;
+
+    public function enableAndForceLameTag(): void;
+
+    public function noReplayGain(): void;
+
+    public function quality(int $quality): void;
+
+    public function resample(float $frequency): void;
+
+    public function bitwidth(int $width): void;
+
+    public function cbr(): void;
+
+    public function bitrate(int $rate): void;
+
+    public function abr(): void;
+
+    public function vbr(int $quality): void;
 }
