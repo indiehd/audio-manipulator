850120d697c3e9a7f8324a61b1dabecc808c2905
Ben Johnson <ben@indiehd.com>
2019-04-12 10:29:11 -0400

    tests: Add tests for lame CLI interactions

--- tests/CliCommand/LameCommandTest.php
+++ tests/CliCommand/LameCommandTest.php
@@ -59,4 +59,104 @@ class LameCommandTest extends TestCase
             $this->lameCommand->asString()
         );
     }
+
+    public function testItComposesQuietCommandCorrectly()
+    {
+        $this->lameCommand->quiet();
+
+        $this->assertEquals(
+            $this->lameCommand->getBinary() . ' --quiet',
+            $this->lameCommand->asString()
+        );
+    }
+
+    public function testItComposesEnableAndForceLameTagCommandCorrectly()
+    {
+        $this->lameCommand->enableAndForceLameTag();
+
+        $this->assertEquals(
+            $this->lameCommand->getBinary() . ' -T',
+            $this->lameCommand->asString()
+        );
+    }
+
+    public function testItComposesNoReplayGainCommandCorrectly()
+    {
+        $this->lameCommand->noReplayGain();
+
+        $this->assertEquals(
+            $this->lameCommand->getBinary() . ' --noreplaygain',
+            $this->lameCommand->asString()
+        );
+    }
+
+    public function testItComposesQualityCommandCorrectly()
+    {
+        $this->lameCommand->quality(0);
+
+        $this->assertEquals(
+            $this->lameCommand->getBinary() . ' --q 0',
+            $this->lameCommand->asString()
+        );
+    }
+
+    public function testItComposesResampleCommandCorrectly()
+    {
+        $this->lameCommand->resample(44);
+
+        $this->assertEquals(
+            $this->lameCommand->getBinary() . ' --resample 44',
+            $this->lameCommand->asString()
+        );
+    }
+
+    public function testItComposesBitwidthCommandCorrectly()
+    {
+        $this->lameCommand->bitwidth(16);
+
+        $this->assertEquals(
+            $this->lameCommand->getBinary() . ' --bitwidth 16',
+            $this->lameCommand->asString()
+        );
+    }
+
+    public function testItComposesCbrCommandCorrectly()
+    {
+        $this->lameCommand->cbr();
+
+        $this->assertEquals(
+            $this->lameCommand->getBinary() . ' --cbr',
+            $this->lameCommand->asString()
+        );
+    }
+
+    public function testItComposesBitrateCommandCorrectly()
+    {
+        $this->lameCommand->bitrate(128);
+
+        $this->assertEquals(
+            $this->lameCommand->getBinary() . ' -b 128',
+            $this->lameCommand->asString()
+        );
+    }
+
+    public function testItComposesAbrCommandCorrectly()
+    {
+        $this->lameCommand->abr();
+
+        $this->assertEquals(
+            $this->lameCommand->getBinary() . ' --abr',
+            $this->lameCommand->asString()
+        );
+    }
+
+    public function testItComposesVbrCommandCorrectly()
+    {
+        $this->lameCommand->vbr(4);
+
+        $this->assertEquals(
+            $this->lameCommand->getBinary() . ' --vbr-new -V 4',
+            $this->lameCommand->asString()
+        );
+    }
 }
