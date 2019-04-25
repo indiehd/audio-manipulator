6275016254433568580649d16b7d277d30ce8fa8
Ben Johnson <ben@indiehd.com>
2019-04-24 22:17:01 -0400

    refact: Eliminate duplication

--- src/BaseManipulator.php
+++ src/BaseManipulator.php
@@ -6,8 +6,25 @@ use IndieHD\AudioManipulator\ManipulatorInterface;
 
 class BaseManipulator implements ManipulatorInterface
 {
+    protected $file;
+
+    public function __construct(string $file)
+    {
+        $this->file = $file;
+    }
+
     public function getFile(): string
     {
         return $this->file;
+    }
+
+    public function writeTags(array $data)
+    {
+        $this->tagger->writeTags($this->file, $data);
+    }
+
+    public function removeTags(array $data)
+    {
+        $this->tagger->removeTags($this->file, $data);
     }
 }
